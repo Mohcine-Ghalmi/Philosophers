48a288d4c5fcd359bdc04621b3db115c15047166
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-05-04 22:25:40 +0100

    testing

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
-#include "philosopher.h"
+#include "philosophers.h"
 
 int main(void)
 {
-    
+    // printf("befor usleep\n");
+    // int us = usleep(3000000); // 3s
+    // printf("%d || after usleep\n", us);
+    // settimeofday()
+    struct timeval *time = NULL;
+    gettimeofday(time, NULL);
 }
