0eb211c829ce336972dcc96f5ad9119c7a94e4f0
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-05-04 22:26:15 +0100

    changinf name adding lib's

---
+++ philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 15:07:11 by mghalmi           #+#    #+#             */
+/*   Updated: 2023/05/04 17:06:08 by mghalmi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#endif
