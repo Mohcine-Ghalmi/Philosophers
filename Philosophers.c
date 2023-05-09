0d91041b1fc313a673049bcf8f4e4a25602028c0
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2023-05-09 20:43:58 +0100

    code parsing

--- Philosophers.c
+++ Philosophers.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.h                                     :+:      :+:    :+:   */
+/*   Philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/29 15:07:11 by mghalmi           #+#    #+#             */
-/*   Updated: 2023/05/09 20:18:01 by mghalmi          ###   ########.fr       */
+/*   Created: 2023/05/08 16:47:33 by mghalmi           #+#    #+#             */
+/*   Updated: 2023/05/08 16:47:57 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
+#include "philosophers.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <pthread.h>
-#include <string.h>
-# define max_value 2147483647
-
-typedef struct philosophers
+int main(int argc, char **argv)
 {
-    long number_of_philosophers;
-    long time_to_die;
-    long time_to_eat;
-    long time_to_sleep;
-    long number_of_times_each_philosopher_must_eat;
-}           t_philo;
-
-#endif
+	
+}
--- test.c
+++ test.c
@@ -1,30 +1,67 @@
 #include "philosophers.h"
 
-void *threadFunc(void *arg)
+long	ft_atoi(const char *str)
 {
-    char *s;
-    static int countThreads = 1;
+	long	res;
+	long	i;
+	int	signe;
 
-    s = (char *)arg;
-    printf("%s %d\n", s, countThreads++);
-    return (void *) strlen(s);
+	res = 0;
+	i = 0;
+	signe = 1;
+	while (str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r'
+		|| str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signe *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		res = res * 10 + str[i++] - '0';
+	return (signe * res);
 }
 
-int main(void)
+int check_args(t_philo *philosophers)
 {
-    pthread_t Thread1;
-    pthread_t Thread2;
-    pthread_t Thread3;
-    void *res;
+    if (philosophers->number_of_philosophers > 0)
+        if (philosophers->time_to_die >= 60 && philosophers->time_to_die <= max_value) // max int
+            if (philosophers->time_to_eat >= 60 && philosophers->time_to_eat <= max_value) // max int
+                if (philosophers->time_to_sleep >= 60 && philosophers->time_to_sleep <= max_value) // max int
+                    return 0;
+    return 1;
+}
 
-    pthread_create(&Thread1, NULL, threadFunc, "i'm The Thread number");
-    pthread_create(&Thread2, NULL, threadFunc, "i'm The Thread number");
-    pthread_create(&Thread3, NULL, threadFunc, "i'm The Thread number");
-    pthread_join(Thread1, &res);
-    pthread_join(Thread2, &res);
-    pthread_join(Thread3, &res);
-    // pthread_detach(pthread_self());
-    // usleep(40000);
-    printf("Threads returned %ld", (long)res);
-    return 0;
+int main(int argc, char **argv)
+{
+    t_philo *philosophers;
+
+    if (argc == 5 || argc == 6)
+    {
+        philosophers = malloc(sizeof(t_philo));
+        philosophers->number_of_philosophers = ft_atoi(argv[1]);
+        philosophers->time_to_die = ft_atoi(argv[2]);
+        philosophers->time_to_eat = ft_atoi(argv[3]);
+        philosophers->time_to_sleep = ft_atoi(argv[4]);
+        if (check_args(philosophers) == 1)
+        {
+            free(philosophers);
+            printf("\033[31minvalid arguments");
+            return (0);
+        }
+        if (argc == 5)
+            printf("philosophres number %ld || die %ld || eat %ld || sleep %ld\n",
+                philosophers->number_of_philosophers, philosophers->time_to_die, philosophers->time_to_eat, philosophers->time_to_sleep);
+        else
+        {
+            philosophers->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+            printf("philosophres number %ld || die %ld || eat %ld || sleep %ld || rotine %ld\n",
+                philosophers->number_of_philosophers, philosophers->time_to_die, philosophers->time_to_eat, philosophers->time_to_sleep, philosophers->number_of_times_each_philosopher_must_eat);
+        }
+    }
+    else
+        printf("\033[31minvalid arguments");
+    return (0);
 }
